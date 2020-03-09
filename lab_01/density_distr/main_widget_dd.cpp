f0ce6cd4bf3cedc098e04785de166e5c570d4133
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-03-09 21:19:12 +0300

    qwidget form is ready

--- lab_01/density_distr/main_widget_dd.cpp
+++ lab_01/density_distr/main_widget_dd.cpp
@@ -7,23 +7,23 @@
 #include <QFormLayout>
 #include <QLabel>
 #include <QLineEdit>
-
+#include <QDoubleValidator>
 
 MainWidgetDD::MainWidgetDD(QWidget * parent):
 QWidget(parent),
-horizontal_layout_(nullptr),
-vertical_layout_(nullptr),
 graphics_view_(nullptr),
 gscene_(nullptr),
-group_box_(nullptr),
 uniform_distr_rbtn_(nullptr),
+exponent_distr_rbtn_(nullptr),
 normal_distr_rbtn_(nullptr),
 poisson_distr_rbtn_(nullptr),
 param_01_label_(nullptr),
+param_02_label_(nullptr),
 param_01_edit_(nullptr),
-
+param_02_edit_(nullptr),
 update_btn_(nullptr),
-clear_btn_(nullptr) {
+clear_btn_(nullptr),
+quit_button_(nullptr) {
 
     // Setup main widget size.
     auto screen_list = QApplication::screens();
@@ -42,68 +42,127 @@ clear_btn_(nullptr) {
     this->move(x_pos, y_pos);
 
     // Create basic layout
-    horizontal_layout_ = new QHBoxLayout();
-    this->setLayout(horizontal_layout_);
+    auto horizontal_layout = new QHBoxLayout();
+    this->setLayout(horizontal_layout);
 
     // PlotGraphicsScene
     gscene_ = new PlotGraphicsScene();
     gscene_->addText("Test GScene");
 
     graphics_view_ = new QGraphicsView(gscene_);
-    horizontal_layout_->addWidget(graphics_view_);
+    graphics_view_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    horizontal_layout->addWidget(graphics_view_, 1);
     graphics_view_->show();
 
     // Create control pannel layout
-    vertical_layout_ = new QVBoxLayout();
-    vertical_layout_->setSizeConstraint(QLayout::SetMinimumSize);
-    horizontal_layout_->addLayout(vertical_layout_);
+    auto vertical_layout = new QVBoxLayout();
+    vertical_layout->setSizeConstraint(QLayout::SetMinimumSize);
+    horizontal_layout->addLayout(vertical_layout);
 
     // Density groupbox
-    group_box_ = new QGroupBox(tr("Распределение"));
-    uniform_distr_rbtn_ = new QRadioButton(tr("Равномерное"));
-    normal_distr_rbtn_  = new QRadioButton(tr("Нормальное"));
-    poisson_distr_rbtn_ = new QRadioButton(tr("Пуассона"));
+    auto group_box = new QGroupBox(tr("Распределение"));
+    uniform_distr_rbtn_  = new QRadioButton(tr("Равномерное"));
+    exponent_distr_rbtn_ = new QRadioButton(tr("Экспоненциальное"));
+    normal_distr_rbtn_   = new QRadioButton(tr("Нормальное"));
+    poisson_distr_rbtn_  = new QRadioButton(tr("Пуассона"));
 
     QVBoxLayout * radio_layout = new QVBoxLayout();
     radio_layout->addWidget(uniform_distr_rbtn_);
+    radio_layout->addWidget(exponent_distr_rbtn_);
     radio_layout->addWidget(normal_distr_rbtn_);
     radio_layout->addWidget(poisson_distr_rbtn_);
-    group_box_->setLayout(radio_layout);
-    vertical_layout_->addWidget(group_box_);
+    group_box->setLayout(radio_layout);
+    vertical_layout->addWidget(group_box);
+    vertical_layout->addSpacing(32);
 
+    // Parameters groupbox
     auto form_group_box = new QGroupBox(tr("Параметры"));
     QVBoxLayout * params_layout = new QVBoxLayout();
 
+    // Parameter 1
     QHBoxLayout * param_01_layout = new QHBoxLayout();
-//    param_01_layout->setSizeConstraint(100);
-    param_01_label_ = new QLabel(tr("undef"));
+    param_01_label_ = new QLabel(tr("Парам_1 = "));
     param_01_edit_  = new QLineEdit();
-//    //param_01_edit_->setMaxLength(2);
-    param_01_edit_->setFixedSize(100,50);
+    param_01_edit_->setFixedSize(100,30);
+    // Parameter 2
+    QHBoxLayout * param_02_layout = new QHBoxLayout();
+    param_02_label_ = new QLabel(tr("Парам_2 = "));
+    param_02_edit_  = new QLineEdit();
+    param_02_edit_->setFixedSize(100,30);
 
-//    param_01_layout->addWidget(param_01_label_);
     param_01_layout->setSizeConstraint(QLayout::SetMinimumSize);
-//    param_01_layout->addStretch();
+    param_01_layout->addWidget(param_01_label_);
     param_01_layout->addWidget(param_01_edit_);
-//            //addRow(param_01_label_, param_01_edit_);
-//    param_01_layout->addStretch();
+    param_01_edit_->setValidator(new QDoubleValidator());
+
+    param_02_layout->setSizeConstraint(QLayout::SetMinimumSize);
+    param_02_layout->addWidget(param_02_label_);
+    param_02_layout->addWidget(param_02_edit_);
+    param_02_edit_->setValidator(new QDoubleValidator());
 
     params_layout->addLayout(param_01_layout);
+    params_layout->addLayout(param_02_layout);
     form_group_box->setLayout(params_layout);
-    vertical_layout_->addWidget(form_group_box);
+    vertical_layout->addWidget(form_group_box);
+    vertical_layout->addSpacing(32);
 
+    // Range GroupBox
+    auto range_group_box = new QGroupBox(tr("Диапазон"));
+    QVBoxLayout * range_layout = new QVBoxLayout();
+    // a_left
+    QHBoxLayout * a_left_layout = new QHBoxLayout();
+    auto a_left_label = new QLabel(tr("a = "));
+    auto a_left_edit  = new QLineEdit();
+    a_left_edit->setFixedSize(100,30);
+    // b_right
+    QHBoxLayout * b_right_layout = new QHBoxLayout();
+    auto b_right_label = new QLabel(tr("b = "));
+    auto b_right_edit  = new QLineEdit();
+    b_right_edit->setFixedSize(100,30);
+
+    a_left_layout->setSizeConstraint(QLayout::SetMinimumSize);
+    a_left_layout->addWidget(a_left_label, 0, Qt::AlignCenter);
+    a_left_layout->addWidget(a_left_edit);
+    a_left_edit->setValidator(new QDoubleValidator());
+
+    b_right_layout->setSizeConstraint(QLayout::SetMinimumSize);
+    b_right_layout->addWidget(b_right_label, 0, Qt::AlignCenter);
+    b_right_layout->addWidget(b_right_edit);
+    b_right_edit->setValidator(new QDoubleValidator());
+
+    range_layout->addLayout(a_left_layout);
+    range_layout->addLayout(b_right_layout);
+    range_group_box->setLayout(range_layout);
+    vertical_layout->addWidget(range_group_box);
+    vertical_layout->addSpacing(32);
 
     update_btn_ = new QPushButton(tr("Добавить график"));
-    vertical_layout_->addWidget(update_btn_);
+    vertical_layout->addWidget(update_btn_);
+    vertical_layout->addSpacing(10);
 
     clear_btn_ = new QPushButton(tr("Очистить"));
-    vertical_layout_->addWidget(clear_btn_);
+    vertical_layout->addWidget(clear_btn_);
 
-    vertical_layout_->addStretch(1);
+    vertical_layout->addStretch(1);
+
+    quit_button_ = new QPushButton(tr("Выход"));
+    vertical_layout->addWidget(quit_button_);
 }
 
 MainWidgetDD::~MainWidgetDD() {
 
+    graphics_view_ = nullptr;
     gscene_ = nullptr;
-    //graphics_panel_layout_ = nullptr;
+    uniform_distr_rbtn_ = nullptr;
+    exponent_distr_rbtn_ = nullptr;
+    normal_distr_rbtn_ = nullptr;
+    poisson_distr_rbtn_ = nullptr;
+    param_01_label_ = nullptr;
+    param_02_label_ = nullptr;
+    param_01_edit_ = nullptr;
+    param_02_edit_ = nullptr;
+    update_btn_ = nullptr;
+    clear_btn_ = nullptr;
+    quit_button_ = nullptr;
 }
--- lab_01/density_distr/main_widget_dd.h
+++ lab_01/density_distr/main_widget_dd.h
@@ -21,27 +21,28 @@ public:
     ~MainWidgetDD();
 
 private:
-    // Auxiliary layouts
-    QHBoxLayout * horizontal_layout_;
-    QVBoxLayout * vertical_layout_;
-
     // Graphical plotting
     QGraphicsView * graphics_view_;
     PlotGraphicsScene * gscene_;
 
-    // Control buttons etc.
-    QGroupBox * group_box_;
+    // Type of the distribution radio buttons etc.
     QRadioButton * uniform_distr_rbtn_;
+    QRadioButton * exponent_distr_rbtn_;
     QRadioButton * normal_distr_rbtn_;
     QRadioButton * poisson_distr_rbtn_;
 
+    // Parameters
     QLabel * param_01_label_;
+    QLabel * param_02_label_;
     QLineEdit * param_01_edit_;
+    QLineEdit * param_02_edit_;
 
+    // Push buttons
     QPushButton * update_btn_;
     QPushButton * clear_btn_;
 
-
+    QPushButton * quit_button_;
 };
 
 #endif // _MAIN_WIDGET_DD_H_
+
